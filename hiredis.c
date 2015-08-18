bfbc90c8618b8f3d6dbedd6c4fe2d3f21f01d5cb
Adam Saponara <asaponara@etsy.com>
2015-08-18 22:40:04 +0000

    add __call

--- hiredis.c
+++ hiredis.c
@@ -26,6 +26,7 @@
 #include "php.h"
 #include "php_ini.h"
 #include "ext/standard/info.h"
+#include "ext/standard/basic_functions.h"
 #include "php_hiredis.h"
 
 #include "main/SAPI.h"
@@ -39,8 +40,14 @@
 
 static zend_object_handlers hiredis_obj_handlers;
 zend_class_entry *hiredis_ce;
+zend_class_entry *hiredis_exception_ce;
 
 ZEND_BEGIN_ARG_INFO_EX(arginfo_hiredis_none, 0, 0, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_hiredis_call, 0, 0, 2)
+    ZEND_ARG_INFO(0, func_name)
+    ZEND_ARG_INFO(0, func_args)
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_INFO_EX(arginfo_hiredis_connect, 0, 0, 2)
@@ -65,12 +72,10 @@ ZEND_BEGIN_ARG_INFO_EX(arginfo_hiredis_set_max_read_buf, 0, 0, 1)
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_INFO_EX(arginfo_hiredis_command, 0, 0, 1)
-    ZEND_ARG_INFO(0, command)
     ZEND_ARG_INFO(0, command_args)
 ZEND_END_ARG_INFO()
 
-ZEND_BEGIN_ARG_INFO_EX(arginfo_hiredis_command_array, 0, 0, 2)
-    ZEND_ARG_INFO(0, command)
+ZEND_BEGIN_ARG_INFO_EX(arginfo_hiredis_command_array, 0, 0, 1)
     ZEND_ARG_INFO(0, command_argv)
 ZEND_END_ARG_INFO()
 
@@ -155,7 +160,12 @@ static void _hiredis_replyobj_nest(const redisReadTask* task, zval* z) {
 /* redisReplyObjectFunctions: Create string */
 static void* hiredis_replyobj_create_string(const redisReadTask* task, char* str, size_t len) {
     zval* z = (zval*)safe_emalloc(1, sizeof(zval), 0);
-    ZVAL_STRINGL(z, str, len);
+    if (task->type == REDIS_REPLY_ERROR) {
+        object_init_ex(z, hiredis_exception_ce);
+        zend_update_property_stringl(hiredis_exception_ce, z, "message", sizeof("message")-1, str, len);
+    } else {
+        ZVAL_STRINGL(z, str, len);
+    }
     _hiredis_replyobj_nest(task, z);
     return (void*)z;
 }
@@ -198,21 +208,84 @@ static redisReplyObjectFunctions hiredis_replyobj_funcs = {
     hiredis_replyobj_free
 };
 
+/* Convert zval of type array to a C array of zvals. Call must free ret_zvals. */
+static void _hiredis_convert_zval_to_array_of_zvals(zval* arr, zval** ret_zvals, int* ret_num_zvals) {
+    zval* zvals;
+    zval* zv;
+    int argc;
+    int i;
+    if (Z_TYPE_P(arr) != IS_ARRAY) {
+        SEPARATE_ZVAL(arr);
+        convert_to_array(arr);
+    }
+    argc = zend_hash_num_elements(Z_ARRVAL_P(arr));
+    zvals = (zval*)safe_emalloc(argc, sizeof(zval), 0);
+    i = 0;
+    ZEND_HASH_FOREACH_VAL(Z_ARRVAL_P(arr), zv) {
+        ZVAL_COPY_VALUE(&zvals[i], zv);
+        i++;
+    } ZEND_HASH_FOREACH_END();
+    *ret_zvals = zvals;
+    *ret_num_zvals = argc;
+}
+
+/* Implementation of Hiredis::__call */
+static void _hiredis_command_array(INTERNAL_FUNCTION_PARAMETERS, hiredis_t* client, char* cmd, zval* args, int argc, int is_append) {
+    char** string_args;
+    size_t* string_lens;
+    int i, j;
+    zval* zv;
+    int num_strings;
+
+    // Convert array of zvals to string + stringlen params. If cmd is not NULL
+    // make it the first arg.
+    num_strings = cmd ? argc + 1 : argc;
+    string_args = (char**)safe_emalloc(num_strings, sizeof(char*), 0);
+    string_lens = (size_t*)safe_emalloc(num_strings, sizeof(size_t), 0);
+    j = 0;
+    if (cmd) {
+        string_args[j] = cmd;
+        string_lens[j] = strlen(cmd);
+        j++;
+    }
+    for (i = 0; i < argc; i++, j++) {
+        convert_to_string_ex(&args[i]);
+        string_args[j] = Z_STRVAL_P(&args[i]);
+        string_lens[j] = Z_STRLEN_P(&args[i]);
+    }
+
+    // Send command to server
+    if (is_append) {
+        if (REDIS_OK != redisAppendCommandArgv(client->ctx, num_strings, (const char**)string_args, string_lens)) {
+            HIREDIS_SET_ERROR(client);
+            RETVAL_FALSE;
+        } else {
+            RETVAL_TRUE;
+        }
+    } else {
+        if (zv = redisCommandArgv(client->ctx, num_strings, (const char**)string_args, string_lens)) {
+            RETVAL_ZVAL(zv, 0, 0);
+        } else {
+            HIREDIS_SET_ERROR(client);//, REDIS_ERR, "redisCommandArgv returned NULL");
+            RETVAL_FALSE;
+        }
+    }
+
+    // Cleanup
+    efree(string_args);
+    efree(string_lens);
+}
+
 /* Implementation of hiredis_(append_)?command(_array)? */
 static void _hiredis_command(INTERNAL_FUNCTION_PARAMETERS, int is_array, int is_append) {
     hiredis_t* client;
     zval* zobj;
     zval* args;
-    zval* newargs = NULL;
-    zval* zv;
     int argc;
-    int i;
-    char** string_args;
 
     if (zend_parse_method_parameters(ZEND_NUM_ARGS(), getThis(), "O+", &zobj, hiredis_ce, &args, &argc) == FAILURE) {
         RETURN_FALSE;
     }
-
     if ((is_array && argc != 1) || (!is_array && argc < 1)) {
         WRONG_PARAM_COUNT;
     }
@@ -220,49 +293,9 @@ static void _hiredis_command(INTERNAL_FUNCTION_PARAMETERS, int is_array, int is_
     client = Z_HIREDIS_P(zobj);
     HIREDIS_ENSURE_CTX(client);
 
-    if (is_array) {
-        zval* arr;
-        arr = &args[0];
-        if (Z_TYPE_P(arr) != IS_ARRAY) {
-            SEPARATE_ZVAL(arr);
-            convert_to_array(arr);
-        }
-        argc = zend_hash_num_elements(Z_ARRVAL_P(arr));
-        newargs = (zval*)safe_emalloc(argc, sizeof(zval), 0);
-        i = 0;
-        ZEND_HASH_FOREACH_VAL(Z_ARRVAL_P(arr), zv) {
-            ZVAL_COPY_VALUE(&newargs[i], zv);
-            i++;
-        } ZEND_HASH_FOREACH_END();
-        args = newargs;
-    }
-
-    string_args = (char**)safe_emalloc(argc, sizeof(char*), 0);
-    for (i = 0; i < argc; i++) {
-        convert_to_string_ex(&args[i]);
-        string_args[i] = Z_STRVAL_P(&args[i]);
-    }
-
-    if (is_append) {
-        if (REDIS_OK != redisAppendCommandArgv(client->ctx, argc, (const char**)string_args, NULL)) {
-            HIREDIS_SET_ERROR(client);
-            RETVAL_FALSE;
-        } else {
-            RETVAL_TRUE;
-        }
-    } else {
-        if (zv = redisCommandArgv(client->ctx, argc, (const char**)string_args, NULL)) {
-            RETVAL_ZVAL(zv, 0, 0);
-        } else {
-            HIREDIS_SET_ERROR_EX(client, REDIS_ERR, "redisCommandArgv returned NULL");
-            RETVAL_FALSE;
-        }
-    }
-
-    if (newargs) {
-        efree(newargs);
-    }
-    efree(string_args);
+    if (is_array) _hiredis_convert_zval_to_array_of_zvals(args, &args, &argc);
+    _hiredis_command_array(INTERNAL_FUNCTION_PARAM_PASSTHRU, client, NULL, args, argc, is_append);
+    if (is_array) efree(args);
 }
 
 /* Invoked after connecting */
@@ -315,6 +348,200 @@ PHP_METHOD(Hiredis, __destruct) {
     return_value = getThis();
     client = Z_HIREDIS_P(return_value);
     _hiredis_conn_deinit(client);
+}
+/* }}} */
+
+/* {{{ proto void Hiredis::__call(string func, array args)
+   Magic command handler. */
+PHP_METHOD(Hiredis, __call) {
+    hiredis_t* client;
+    char* func;
+    size_t func_len;
+    zval* func_args;
+    int func_argc;
+
+    if (zend_parse_parameters(ZEND_NUM_ARGS(), "sa", &func, &func_len, &func_args) == FAILURE) {
+        RETURN_FALSE;
+    }
+
+    client = Z_HIREDIS_P(getThis());
+    HIREDIS_ENSURE_CTX(client);
+
+    _hiredis_convert_zval_to_array_of_zvals(func_args, &func_args, &func_argc);
+
+    #define PHP_HIREDIS_ELSE_IF_CALL(pfunc, pcmd) \
+        } else if (0 == strcasecmp(func, (pfunc))) { \
+            _hiredis_command_array(INTERNAL_FUNCTION_PARAM_PASSTHRU, client, (pcmd), func_args, func_argc, 0)
+
+    if (0) {
+    PHP_HIREDIS_ELSE_IF_CALL("append", "APPEND");
+    PHP_HIREDIS_ELSE_IF_CALL("auth", "AUTH");
+    PHP_HIREDIS_ELSE_IF_CALL("bgrewriteaof", "BGREWRITEAOF");
+    PHP_HIREDIS_ELSE_IF_CALL("bgsave", "BGSAVE");
+    PHP_HIREDIS_ELSE_IF_CALL("bitcount", "BITCOUNT");
+    PHP_HIREDIS_ELSE_IF_CALL("bitop", "BITOP");
+    PHP_HIREDIS_ELSE_IF_CALL("bitpos", "BITPOS");
+    PHP_HIREDIS_ELSE_IF_CALL("blpop", "BLPOP");
+    PHP_HIREDIS_ELSE_IF_CALL("brpop", "BRPOP");
+    PHP_HIREDIS_ELSE_IF_CALL("brpoplpush", "BRPOPLPUSH");
+    PHP_HIREDIS_ELSE_IF_CALL("client", "CLIENT");
+    PHP_HIREDIS_ELSE_IF_CALL("cluster", "CLUSTER");
+    PHP_HIREDIS_ELSE_IF_CALL("command", "COMMAND");
+    PHP_HIREDIS_ELSE_IF_CALL("config", "CONFIG");
+    PHP_HIREDIS_ELSE_IF_CALL("dbsize", "DBSIZE");
+    PHP_HIREDIS_ELSE_IF_CALL("debug", "DEBUG");
+    PHP_HIREDIS_ELSE_IF_CALL("decr", "DECR");
+    PHP_HIREDIS_ELSE_IF_CALL("decrby", "DECRBY");
+    PHP_HIREDIS_ELSE_IF_CALL("del", "DEL");
+    PHP_HIREDIS_ELSE_IF_CALL("discard", "DISCARD");
+    PHP_HIREDIS_ELSE_IF_CALL("dump", "DUMP");
+    PHP_HIREDIS_ELSE_IF_CALL("echo", "ECHO");
+    PHP_HIREDIS_ELSE_IF_CALL("eval", "EVAL");
+    PHP_HIREDIS_ELSE_IF_CALL("evalsha", "EVALSHA");
+    PHP_HIREDIS_ELSE_IF_CALL("exec", "EXEC");
+    PHP_HIREDIS_ELSE_IF_CALL("exists", "EXISTS");
+    PHP_HIREDIS_ELSE_IF_CALL("expire", "EXPIRE");
+    PHP_HIREDIS_ELSE_IF_CALL("expireat", "EXPIREAT");
+    PHP_HIREDIS_ELSE_IF_CALL("flushall", "FLUSHALL");
+    PHP_HIREDIS_ELSE_IF_CALL("flushdb", "FLUSHDB");
+    PHP_HIREDIS_ELSE_IF_CALL("geoadd", "GEOADD");
+    PHP_HIREDIS_ELSE_IF_CALL("geodist", "GEODIST");
+    PHP_HIREDIS_ELSE_IF_CALL("geohash", "GEOHASH");
+    PHP_HIREDIS_ELSE_IF_CALL("geopos", "GEOPOS");
+    PHP_HIREDIS_ELSE_IF_CALL("georadius", "GEORADIUS");
+    PHP_HIREDIS_ELSE_IF_CALL("georadiusbymember", "GEORADIUSBYMEMBER");
+    PHP_HIREDIS_ELSE_IF_CALL("get", "GET");
+    PHP_HIREDIS_ELSE_IF_CALL("getbit", "GETBIT");
+    PHP_HIREDIS_ELSE_IF_CALL("getrange", "GETRANGE");
+    PHP_HIREDIS_ELSE_IF_CALL("getset", "GETSET");
+    PHP_HIREDIS_ELSE_IF_CALL("hdel", "HDEL");
+    PHP_HIREDIS_ELSE_IF_CALL("hexists", "HEXISTS");
+    PHP_HIREDIS_ELSE_IF_CALL("hget", "HGET");
+    PHP_HIREDIS_ELSE_IF_CALL("hgetall", "HGETALL");
+    PHP_HIREDIS_ELSE_IF_CALL("hincrby", "HINCRBY");
+    PHP_HIREDIS_ELSE_IF_CALL("hincrbyfloat", "HINCRBYFLOAT");
+    PHP_HIREDIS_ELSE_IF_CALL("hkeys", "HKEYS");
+    PHP_HIREDIS_ELSE_IF_CALL("hlen", "HLEN");
+    PHP_HIREDIS_ELSE_IF_CALL("hmget", "HMGET");
+    PHP_HIREDIS_ELSE_IF_CALL("hmset", "HMSET");
+    PHP_HIREDIS_ELSE_IF_CALL("hscan", "HSCAN");
+    PHP_HIREDIS_ELSE_IF_CALL("hset", "HSET");
+    PHP_HIREDIS_ELSE_IF_CALL("hsetnx", "HSETNX");
+    PHP_HIREDIS_ELSE_IF_CALL("hstrlen", "HSTRLEN");
+    PHP_HIREDIS_ELSE_IF_CALL("hvals", "HVALS");
+    PHP_HIREDIS_ELSE_IF_CALL("incr", "INCR");
+    PHP_HIREDIS_ELSE_IF_CALL("incrby", "INCRBY");
+    PHP_HIREDIS_ELSE_IF_CALL("incrbyfloat", "INCRBYFLOAT");
+    PHP_HIREDIS_ELSE_IF_CALL("info", "INFO");
+    PHP_HIREDIS_ELSE_IF_CALL("keys", "KEYS");
+    PHP_HIREDIS_ELSE_IF_CALL("lastsave", "LASTSAVE");
+    PHP_HIREDIS_ELSE_IF_CALL("lindex", "LINDEX");
+    PHP_HIREDIS_ELSE_IF_CALL("linsert", "LINSERT");
+    PHP_HIREDIS_ELSE_IF_CALL("llen", "LLEN");
+    PHP_HIREDIS_ELSE_IF_CALL("lpop", "LPOP");
+    PHP_HIREDIS_ELSE_IF_CALL("lpush", "LPUSH");
+    PHP_HIREDIS_ELSE_IF_CALL("lpushx", "LPUSHX");
+    PHP_HIREDIS_ELSE_IF_CALL("lrange", "LRANGE");
+    PHP_HIREDIS_ELSE_IF_CALL("lrem", "LREM");
+    PHP_HIREDIS_ELSE_IF_CALL("lset", "LSET");
+    PHP_HIREDIS_ELSE_IF_CALL("ltrim", "LTRIM");
+    PHP_HIREDIS_ELSE_IF_CALL("mget", "MGET");
+    PHP_HIREDIS_ELSE_IF_CALL("migrate", "MIGRATE");
+    PHP_HIREDIS_ELSE_IF_CALL("monitor", "MONITOR");
+    PHP_HIREDIS_ELSE_IF_CALL("move", "MOVE");
+    PHP_HIREDIS_ELSE_IF_CALL("mset", "MSET");
+    PHP_HIREDIS_ELSE_IF_CALL("msetnx", "MSETNX");
+    PHP_HIREDIS_ELSE_IF_CALL("multi", "MULTI");
+    PHP_HIREDIS_ELSE_IF_CALL("object", "OBJECT");
+    PHP_HIREDIS_ELSE_IF_CALL("persist", "PERSIST");
+    PHP_HIREDIS_ELSE_IF_CALL("pexpire", "PEXPIRE");
+    PHP_HIREDIS_ELSE_IF_CALL("pexpireat", "PEXPIREAT");
+    PHP_HIREDIS_ELSE_IF_CALL("pfadd", "PFADD");
+    PHP_HIREDIS_ELSE_IF_CALL("pfcount", "PFCOUNT");
+    PHP_HIREDIS_ELSE_IF_CALL("pfmerge", "PFMERGE");
+    PHP_HIREDIS_ELSE_IF_CALL("ping", "PING");
+    PHP_HIREDIS_ELSE_IF_CALL("psetex", "PSETEX");
+    PHP_HIREDIS_ELSE_IF_CALL("psubscribe", "PSUBSCRIBE");
+    PHP_HIREDIS_ELSE_IF_CALL("pttl", "PTTL");
+    PHP_HIREDIS_ELSE_IF_CALL("publish", "PUBLISH");
+    PHP_HIREDIS_ELSE_IF_CALL("pubsub", "PUBSUB");
+    PHP_HIREDIS_ELSE_IF_CALL("punsubscribe", "PUNSUBSCRIBE");
+    PHP_HIREDIS_ELSE_IF_CALL("quit", "QUIT");
+    PHP_HIREDIS_ELSE_IF_CALL("randomkey", "RANDOMKEY");
+    PHP_HIREDIS_ELSE_IF_CALL("rename", "RENAME");
+    PHP_HIREDIS_ELSE_IF_CALL("renamenx", "RENAMENX");
+    PHP_HIREDIS_ELSE_IF_CALL("restore", "RESTORE");
+    PHP_HIREDIS_ELSE_IF_CALL("role", "ROLE");
+    PHP_HIREDIS_ELSE_IF_CALL("rpop", "RPOP");
+    PHP_HIREDIS_ELSE_IF_CALL("rpoplpush", "RPOPLPUSH");
+    PHP_HIREDIS_ELSE_IF_CALL("rpush", "RPUSH");
+    PHP_HIREDIS_ELSE_IF_CALL("rpushx", "RPUSHX");
+    PHP_HIREDIS_ELSE_IF_CALL("sadd", "SADD");
+    PHP_HIREDIS_ELSE_IF_CALL("save", "SAVE");
+    PHP_HIREDIS_ELSE_IF_CALL("scan", "SCAN");
+    PHP_HIREDIS_ELSE_IF_CALL("scard", "SCARD");
+    PHP_HIREDIS_ELSE_IF_CALL("script", "SCRIPT");
+    PHP_HIREDIS_ELSE_IF_CALL("sdiff", "SDIFF");
+    PHP_HIREDIS_ELSE_IF_CALL("sdiffstore", "SDIFFSTORE");
+    PHP_HIREDIS_ELSE_IF_CALL("select", "SELECT");
+    PHP_HIREDIS_ELSE_IF_CALL("set", "SET");
+    PHP_HIREDIS_ELSE_IF_CALL("setbit", "SETBIT");
+    PHP_HIREDIS_ELSE_IF_CALL("setex", "SETEX");
+    PHP_HIREDIS_ELSE_IF_CALL("setnx", "SETNX");
+    PHP_HIREDIS_ELSE_IF_CALL("setrange", "SETRANGE");
+    PHP_HIREDIS_ELSE_IF_CALL("shutdown", "SHUTDOWN");
+    PHP_HIREDIS_ELSE_IF_CALL("sinter", "SINTER");
+    PHP_HIREDIS_ELSE_IF_CALL("sinterstore", "SINTERSTORE");
+    PHP_HIREDIS_ELSE_IF_CALL("sismember", "SISMEMBER");
+    PHP_HIREDIS_ELSE_IF_CALL("slaveof", "SLAVEOF");
+    PHP_HIREDIS_ELSE_IF_CALL("slowlog", "SLOWLOG");
+    PHP_HIREDIS_ELSE_IF_CALL("smembers", "SMEMBERS");
+    PHP_HIREDIS_ELSE_IF_CALL("smove", "SMOVE");
+    PHP_HIREDIS_ELSE_IF_CALL("sort", "SORT");
+    PHP_HIREDIS_ELSE_IF_CALL("spop", "SPOP");
+    PHP_HIREDIS_ELSE_IF_CALL("srandmember", "SRANDMEMBER");
+    PHP_HIREDIS_ELSE_IF_CALL("srem", "SREM");
+    PHP_HIREDIS_ELSE_IF_CALL("sscan", "SSCAN");
+    PHP_HIREDIS_ELSE_IF_CALL("strlen", "STRLEN");
+    PHP_HIREDIS_ELSE_IF_CALL("subscribe", "SUBSCRIBE");
+    PHP_HIREDIS_ELSE_IF_CALL("sunion", "SUNION");
+    PHP_HIREDIS_ELSE_IF_CALL("sunionstore", "SUNIONSTORE");
+    PHP_HIREDIS_ELSE_IF_CALL("sync", "SYNC");
+    PHP_HIREDIS_ELSE_IF_CALL("time", "TIME");
+    PHP_HIREDIS_ELSE_IF_CALL("ttl", "TTL");
+    PHP_HIREDIS_ELSE_IF_CALL("type", "TYPE");
+    PHP_HIREDIS_ELSE_IF_CALL("unsubscribe", "UNSUBSCRIBE");
+    PHP_HIREDIS_ELSE_IF_CALL("unwatch", "UNWATCH");
+    PHP_HIREDIS_ELSE_IF_CALL("wait", "WAIT");
+    PHP_HIREDIS_ELSE_IF_CALL("watch", "WATCH");
+    PHP_HIREDIS_ELSE_IF_CALL("zadd", "ZADD");
+    PHP_HIREDIS_ELSE_IF_CALL("zcard", "ZCARD");
+    PHP_HIREDIS_ELSE_IF_CALL("zcount", "ZCOUNT");
+    PHP_HIREDIS_ELSE_IF_CALL("zincrby", "ZINCRBY");
+    PHP_HIREDIS_ELSE_IF_CALL("zinterstore", "ZINTERSTORE");
+    PHP_HIREDIS_ELSE_IF_CALL("zlexcount", "ZLEXCOUNT");
+    PHP_HIREDIS_ELSE_IF_CALL("zrange", "ZRANGE");
+    PHP_HIREDIS_ELSE_IF_CALL("zrangebylex", "ZRANGEBYLEX");
+    PHP_HIREDIS_ELSE_IF_CALL("zrangebyscore", "ZRANGEBYSCORE");
+    PHP_HIREDIS_ELSE_IF_CALL("zrank", "ZRANK");
+    PHP_HIREDIS_ELSE_IF_CALL("zrem", "ZREM");
+    PHP_HIREDIS_ELSE_IF_CALL("zremrangebylex", "ZREMRANGEBYLEX");
+    PHP_HIREDIS_ELSE_IF_CALL("zremrangebyrank", "ZREMRANGEBYRANK");
+    PHP_HIREDIS_ELSE_IF_CALL("zremrangebyscore", "ZREMRANGEBYSCORE");
+    PHP_HIREDIS_ELSE_IF_CALL("zrevrange", "ZREVRANGE");
+    PHP_HIREDIS_ELSE_IF_CALL("zrevrangebylex", "ZREVRANGEBYLEX");
+    PHP_HIREDIS_ELSE_IF_CALL("zrevrangebyscore", "ZREVRANGEBYSCORE");
+    PHP_HIREDIS_ELSE_IF_CALL("zrevrank", "ZREVRANK");
+    PHP_HIREDIS_ELSE_IF_CALL("zscan", "ZSCAN");
+    PHP_HIREDIS_ELSE_IF_CALL("zscore", "ZSCORE");
+    PHP_HIREDIS_ELSE_IF_CALL("zunionstore", "ZUNIONSTORE");
+    } else {
+        zend_throw_error(NULL, "Call to undefined method Hiredis::%s()", func);
+    }
+
+    #undef PHP_HIREDIS_ELSE_IF_CALL
+
+    efree(func_args);
 }
 /* }}} */
 
@@ -568,6 +795,7 @@ PHP_FUNCTION(hiredis_get_last_error) {
 zend_function_entry hiredis_methods[] = {
     PHP_ME(Hiredis, __construct, arginfo_hiredis_none, ZEND_ACC_CTOR | ZEND_ACC_PUBLIC)
     PHP_ME(Hiredis, __destruct,  arginfo_hiredis_none, ZEND_ACC_PUBLIC)
+    PHP_ME(Hiredis, __call,      arginfo_hiredis_call, ZEND_ACC_PUBLIC)
     PHP_ME_MAPPING(connect,              hiredis_connect,              arginfo_hiredis_connect,            ZEND_ACC_PUBLIC)
     PHP_ME_MAPPING(connectUnix,          hiredis_connect_unix,         arginfo_hiredis_connect_unix,       ZEND_ACC_PUBLIC)
     PHP_ME_MAPPING(setTimeout,           hiredis_set_timeout,          arginfo_hiredis_set_timeout,        ZEND_ACC_PUBLIC)
@@ -605,12 +833,16 @@ PHP_MINFO_FUNCTION(hiredis) {
 /* {{{ PHP_MINIT_FUNCTION */
 PHP_MINIT_FUNCTION(hiredis) {
     zend_class_entry ce;
+
     INIT_CLASS_ENTRY(ce, "Hiredis", hiredis_methods);
     hiredis_ce = zend_register_internal_class(&ce);
     hiredis_ce->create_object = hiredis_obj_new;
     memcpy(&hiredis_obj_handlers, zend_get_std_object_handlers(), sizeof(hiredis_obj_handlers));
     hiredis_obj_handlers.offset = XtOffsetOf(hiredis_t, std);
     hiredis_obj_handlers.free_obj = hiredis_obj_free;
+
+    INIT_CLASS_ENTRY(ce, "HiredisException", NULL);
+    hiredis_exception_ce = zend_register_internal_class_ex(&ce, zend_ce_exception);
 }
 /* }}} */
 
---
+++ tests/009_commands_3.phpt
@@ -0,0 +1,18 @@
+--TEST--
+Check Hiredis::(set|get|...)*
+--SKIPIF--
+<?php if (!extension_loaded("hiredis") || (int)shell_exec('netstat -tnlp | grep 6379 | grep redis-server | wc -l') < 1) print "skip"; ?>
+--FILE--
+<?php
+$h = new Hiredis();
+var_dump($h->connect('localhost', 6379));
+var_dump($h->client('SETNAME', 'Bob'));
+var_dump($h->client('GETNAME'));
+var_dump($h->set('quux2', "binary\x00safe"));
+var_dump($h->strlen('quux2'));
+--EXPECT--
+bool(true)
+string(2) "OK"
+string(3) "Bob"
+string(2) "OK"
+int(11)
